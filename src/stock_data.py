ff1e3d7d0ceee40e32a938ced30cfc7f827968dc
Shreyash Bajpai <shreyashbajpai0@gmail.com>
2026-03-26 00:34:41 +0530

    updated name of function and ticker in stock_data.py

--- src/stock_data.py
+++ src/stock_data.py
@@ -2,24 +2,23 @@ import yfinance as yf
 import pandas as pd
 import os
 import datetime as dt
-companys=['TCS.NS','INFY.NS','WIPRO.NS','AAPL','GOOGL','MSFT','AMZN']
 
-def get_data_stock(tickr):
+def get_stock_data(ticker):
         
     curr_date=dt.date.today()
         
-    data=yf.download(tickr,start='01-01-2000',end=curr_date)
+    data=yf.download(ticker,start='01-01-2000',end=curr_date)
         
     if isinstance(data.columns,pd.MultiIndex):
         data.columns=data.columns.get_level_values(0)
         
     data.reset_index(inplace=True)
 
-    folderpath=f'../../data/raw/{tickr}'
+    folderpath=f'../../data/raw/{ticker}'
     os.makedirs(folderpath,exist_ok=True)
 
-    filepath=f'{folderpath}/{tickr}_stock_data.csv'
+    filepath=f'{folderpath}/{ticker}_stock_data.csv'
     data.to_csv(filepath,index=False)
         
-    print(f'the data is saved in {tickr} file')
+    print(f'the data is saved in {ticker} file')
 
